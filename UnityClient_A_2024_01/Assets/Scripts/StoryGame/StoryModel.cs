db77e18cef69df86f4b9f77093ab5d72d1a70e6b
jungmin0923 <wjdals7631@gmail.com>
2024-04-30 10:05:48 +0900

    [Feat] 게임 틀 정리 시작

---
+++ UnityClient_A_2024_01/Assets/Scripts/StoryGame/StoryModel.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using STORYGAME;
+
+[CreateAssetMenu(fileName = "NewStory", menuName = "ScriptableObjects/StoryModel")]
+public class StoryModel : MonoBehaviour
+{
+    public int storyNumber;         //스토리 번호
+    public Texture2D MainImage;     //스토리 보여줄 이미지 텍스쳐
+
+    public enum STORYTYPE           //스토리 타입 설정
+    {
+        MAIN,
+        SUB,
+        SERIAL
+    }
+
+    public STORYTYPE storytype;      //스토리 타입 선언
+    public bool storyDone;           //스토리 종료 여부
+
+    [TextArea(10, 10)]      //텍스트 영역 표시
+    public string stoyText;          //메인 스토리
+
+    public Option[] options;        //선택지 배열
+
+    [System.Serializable]
+    public class Option
+    {
+        public string optionText;
+        public string buttonText;
+        public EventCheck eventCheck;
+    }
+
+    [System.Serializable]
+    public class EventCheck
+    {
+        public int checkValue;
+        public enum EventType : int
+        {
+            NONE,
+            GoToBattle,
+            CheckSTR,
+            CheckDEX,
+            CheckCON,
+            CheckINT,
+            CheckWIS,
+            CheckCHA
+        }
+
+        public EventType eventType;
+
+        public Result[] suceessResult;
+        public Result[] failResult;
+    }
+
+    [System.Serializable]
+    public class Result
+    {
+       public enum ResultType : int
+        {
+            ChangeHp,
+            ChangeSp,
+            AddExperience,
+            GoToShop,
+            GoToNextStory,
+            GoToRandomStory,
+            GoToEnding
+        }
+
+        public ResultType resultType;
+        public int value;
+        public Stats stats;
+    }
+
+}
